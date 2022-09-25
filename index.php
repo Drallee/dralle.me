991f27a1f1d526ced3c011907de76bf3453e58b0
Drallee <61922984+Drallee@users.noreply.github.com>
2022-09-26 00:47:11 +0200

    Summary
    I added main.css, and main.js
    
    I added a little of the css for the header
    and added a nav bar to the index.php

--- index.php
+++ index.php
@@ -25,43 +25,48 @@
     $page_color_hex = random_color();
 
 ?>
-<html>
-    
+<html> 
 <head>
-        <!-- What characters are allowed on the site -->
-	    <meta charset="UTF-8">
-        <!-- The title of the page -->
-		<title><?php echo $page_title; ?></title>
-        <!-- Adds a little icon next to the title in the page tab on the browser -->
-        <link rel="icon" href="<?php echo $page_fav_icon; ?>" type="image/icon" sizes="16x16">
-        <!-- Keywords the people can search for to find this page -->
-		<meta name="keywords" content="<?php echo $page_keywords; ?>">
-		<!-- The author of the website -->
-        <meta name="author" content="<?php echo $page_author; ?>">
-        <!-- The description of the current page -->
-        <meta name="description" content="<?php echo $page_description; ?>">
-        <!-- This makes the website viewable on any device -->
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- This makes the website viewable on any device -->
-		<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-        <!-- These og meta tags help with embeds in chat rooms on other apps -->
-		<meta property="og:title" content="<?php echo $page_title; ?>">
-		<meta property="og:site_name" content="dralle.me" >
-		<meta property="og:type" content="website">
-		<meta property="og:url" content="<?php echo $page_link; ?>">
-		<meta property="og:image" content="<?php echo $page_image; ?>">
-		<meta property="og:description" content="<?php echo $page_description; ?>">
-		<meta name="theme-color" content="<?php echo $page_color_hex; ?>">
-        <!-- Here you will find all the css the page uses -->
-		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-        <!-- These are the scripts the page uses -->
-		<script src="assets/scripts/script.js" async defer></script>
-	</head>
-
+    <!-- What characters are allowed on the site -->
+	<meta charset="UTF-8">
+    <!-- The title of the page -->
+	<title><?php echo $page_title; ?></title>
+    <!-- Adds a little icon next to the title in the page tab on the browser -->
+    <link rel="icon" href="<?php echo $page_fav_icon; ?>" type="image/icon" sizes="16x16">
+    <!-- Keywords the people can search for to find this page -->
+	<meta name="keywords" content="<?php echo $page_keywords; ?>">
+	<!-- The author of the website -->
+    <meta name="author" content="<?php echo $page_author; ?>">
+    <!-- The description of the current page -->
+    <meta name="description" content="<?php echo $page_description; ?>">
+    <!-- This makes the website viewable on any device -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- This makes the website viewable on any device -->
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <!-- These og meta tags help with embeds in chat rooms on other apps -->
+	<meta property="og:title" content="<?php echo $page_title; ?>">
+	<meta property="og:site_name" content="dralle.me" >
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="<?php echo $page_link; ?>">
+	<meta property="og:image" content="<?php echo $page_image; ?>">
+	<meta property="og:description" content="<?php echo $page_description; ?>">
+	<meta name="theme-color" content="<?php echo $page_color_hex; ?>">
+    <!-- Here you will find all the css the page uses -->
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <!-- These are the scripts the page uses -->
+	<script src="assets/scripts/main.js" async defer></script>
+</head>
 <body>
-
-    
-
+    <header>
+        <img class="logo" src="assets/images/logo.svg" alt="logo">
+        <nav>
+            <ul class="nav__links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">New menus</a></li>
+                <li><a href="#">Old menus</a></li>
+            </ul>
+        </nav>
+        <a class="cta" href="#"><button>Login/Signup</button></a>
+    </header>
 </body>
-
 </html>
